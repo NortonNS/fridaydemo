ba839f9485d67da0a9552d7c79648122d9060861
NortonNS <martin.j.power@gmail.com>
2021-02-24 12:59:10 -0400

    Changed for unit testing.

---
+++ src/test/java/com/example/fridaydemo/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.fridaydemo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void index() {
+    }
+}
--- src/test/java/com/example/fridaydemo/myFirstRestAssuredClass.java
+++ src/test/java/com/example/fridaydemo/myFirstRestAssuredClass.java
@@ -1,6 +1,12 @@
 package com.example.fridaydemo;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 import static io.restassured.RestAssured.get;
@@ -9,18 +15,28 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.hamcrest.Matchers.hasSize;
 
 public class myFirstRestAssuredClass {
-    final static String url="http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1";
-    final static String irl="http://demo.guru99.com/V4/sinkministatement.php";
-    final static long responseTimeThreshold = 500; // milliseconds
+    //final static String url="http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1";
+    final static String uri="http://demo.guru99.com/V4/sinkministatement.php";
+    final static long responseTimeThreshold = 1000; // milliseconds
     final static int successfulStatusCode = 200;
+    private static RequestSpecification spec;
 
-    public static void main(String[] args) {
-    	getTesting();
-        getResponseStatus();
-        getResponseTime();
+    // Initialize a Specification to be used in other JUNIT Testing
+    @BeforeAll
+    public static void initSpec() {
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("http://demo.guru99.com/")
+                .addFilter(new ResponseLoggingFilter()) //log request and response for better debugging. You can also only log if a requests fails.
+                .addFilter(new RequestLoggingFilter())
+                .build();
+        System.out.println("****************************************************");
+
     }
 
-    public static void getTesting() {
+    // Use pathParam - show how you can pass parameters to the URL path.
+    @Test
+    public void getTesting() {
     	given()
                 .pathParam("raceSeason","2017")
                 .when()
@@ -33,21 +49,26 @@ public class myFirstRestAssuredClass {
                 and()
                     .body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
     }
-    
-    public static void getResponseStatus(){
-        int statusCode= given().queryParam("CUSTOMER_ID","68195")
+
+    // use spec above to set the initial url, content type, etc.
+    @Test
+    public void getResponseStatus(){
+        given()
+                .spec(spec)
+                .queryParam("CUSTOMER_ID","68195")
                 .queryParam("PASSWORD","1234!")
                 .queryParam("Account_No","1")
-                .when().get(irl).getStatusCode();
-        System.out.println("The response status is "+statusCode);
-
-        Assert.isTrue(statusCode == successfulStatusCode,
-                "Status Code in incorrect, was expecting " + successfulStatusCode);
+                .when()
+                .get("V4/sinkministatement.php")
+                .then()
+                .statusCode(successfulStatusCode);
+                //.and().contentType(ContentType.BINARY);
     }
 
     // Check to ensure response time is within acceptable limits.
-    public static void getResponseTime()  {
-        long responseTime = get(url).timeIn(MILLISECONDS);
+    @Test
+    public void getResponseTime()  {
+        long responseTime = get(uri).timeIn(MILLISECONDS);
         System.out.println("The time taken to fetch the response "+ responseTime + " milliseconds");
 
         Assert.isTrue(responseTime < responseTimeThreshold, "Response Time Exceeded");
